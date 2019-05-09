7d5f41776ee7dc968a703f496696144d2af6dd07
Hu Jiawen <jw.hu@s2icode.com>
2019-05-09 17:07:49 +0800

    书后习题 4-1 a 修正

--- HUOpenCVOCTool/HUOpenCVOCTool/HUFourthChapterAnswer.cpp
+++ HUOpenCVOCTool/HUOpenCVOCTool/HUFourthChapterAnswer.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "HUFourthChapterAnswer.hpp"
+#include <iostream>
 #include <highgui.h>
 #include <cv.h>
 /*
@@ -14,27 +15,62 @@
  */
 
 void fourth_first() {
-    CvCapture* video = cvCreateFileCapture("/Users/mac/Desktop/douyin.mpg");
+//    CvCapture* video = cvCreateFileCapture("/Users/jw.hu/Desktop/tree.avi");
 //    CvCapture* video = cvCreateCameraCapture(0);
-    CV_Assert(video);
-    IplImage* frame = cvQueryFrame(video);
+//    CV_Assert(video);
     
-    IplImage* grey = cvCreateImage(cvGetSize(frame), frame->depth, 1);
-    cvConvertImage(frame, grey);
+//    IplImage* frame = cvQueryFrame(video);
+    IplImage* img = cvLoadImage("/Users/jw.hu/Desktop/OpenCV_Source/smarties.png");
+    IplImage* grey = cvCreateImage(cvGetSize(img), img->depth, 1);
+    IplImage* canny = cvCloneImage(grey);
+    // 转成灰度图片
+    cvConvertImage(img, grey);
+    // Canndy 边缘检测
+    cvCanny(grey, canny, 100, 200);
+    // 显示图片
+    cvNamedWindow("Ex4-1 A");
+    cvNamedWindow("Ex4-1 B");
+    cvNamedWindow("Ex4-1 C");
+    
+    cvShowImage("Ex4-1 A", img);
+    cvShowImage("Ex4-1 B", grey);
+    cvShowImage("Ex4-1 C", canny);
+    
+    cvMoveWindow("Ex4-1 A", 0, 0);
+    cvMoveWindow("Ex4-1 B", img->width, 0);
+    cvMoveWindow("Ex4-1 C", img->width * 2, 0);
+
+    
+    while (1) { if (cvWaitKey(100) == 27) { break; } }
+    cvDestroyAllWindows();
+}
+// 4-2
+void showpiexl(IplImage* img, int x, int y);
+void mouseCallBack( int event, int x, int y, int flags, void* param) {
+    IplImage* img0;
+    img0 = (IplImage *)param;
+    switch (event) {
+        case CV_EVENT_LBUTTONUP:
+            
+            //
+            showpiexl(img0, x, y);
+            break;
+            
+        default:
+            break;
+    }
 }
 
-void fourth_my_mouse_callback( int event, int x, int y, int flags, void* param);
 
 void fourth_second(const char* fileName) {
     
     IplImage* image = cvLoadImage(fileName);
     
     cvNamedWindow("EX4-2");
-    cvShowImage("Ex4-2", image);
+    cvShowImage("EX4-2", image);
    
-    cvSetMouseCallback("EX4-2", fourth_my_mouse_callback, (void *)image);
-    
-    cvWaitKey();
+    cvSetMouseCallback("EX4-2", mouseCallBack, (void *)image);
+    while (1) { if (cvWaitKey(100) == 27) { break; } }
     cvDestroyAllWindows();
     cvReleaseImage(&image);
 }
@@ -46,17 +82,4 @@ void showpiexl(IplImage* img, int x, int y) {
     CvFont font;
     cvInitFont(&font, CV_FONT_VECTOR0, 1, 1, 0, 1, 8);
     cvShowImage("EX4-2", img);
-}
-
-void fourth_my_mouse_callback(int event, int x, int y, int flags, void* param) {
-    IplImage* img0;
-    img0 = (IplImage *)param;
-    switch (event) {
-        case CV_EVENT_LBUTTONUP:
-            showpiexl(img0, x, y);
-            break;
-            
-        default:
-            break;
-    }
 }
