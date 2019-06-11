6d86c0e3a1a7b29674c6ade34186e9ef4513a4e1
有鬼啊啊啊啊 <644727408@qq.com>
2019-06-11 23:11:08 +0800

    添加识别矩形方法

--- HUOpenCVOCTool/HUOpenCVOCTool/S2iReflective.cpp
+++ HUOpenCVOCTool/HUOpenCVOCTool/S2iReflective.cpp
@@ -30,7 +30,8 @@ int brightness_mat(const Mat &mat) {
 
 int calculate_mat_brightness(const Mat mat, vector<Point> points) {
     Mat mask, copyImg;
-    mask = Mat::zeros(mat.size(), CV_8UC1);
+    mask = Mat::zeros(mat.size(), CV_8U);
+    
     
     int width = mat.cols;
     int height = mat.rows;
@@ -83,18 +84,112 @@ void imageClarity() {
     
 }
 
+bool isCntValid(vector<Point> cnt, int rows, int cols) {
+    for (Point point : cnt) {
+        if (point.x == 0 || point.y == 0 || point.x == rows - 1 || point.y == cols - 1) {
+            return false;
+        }
+    }
+    return true;
+}
+// 计算两点之间的距离
+double distanceOfPoint(Point p1, Point p2) {
+    double x = abs(p1.x - p2.x);
+    double y = abs(p1.y - p2.y);
+    return sqrt(pow(x, 2) + pow(y, 2));
+}
+
+bool isCntScaleValid(vector<Point> cnt) {
+    if (cnt.size() == 4) {
+        Point p0 = cnt[0];
+        Point p1 = cnt[1];
+        Point p2 = cnt[2];
+        double width = floor(distanceOfPoint(p0, p1));
+        double height = floor(distanceOfPoint(p1, p2));
+        double scale = width / height;
+        if (scale > 0.7 && scale < 1.4) {
+            return true;
+        }
+    }
+    return false;
+}
+
+double angleOfPoint(Point pt1, Point pt2, Point pt0) {
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
+void traverse_points_find_square(const Mat gray, vector<vector<Point> > contours, vector<vector<Point> > &squares) {
+    // 找到形状的边缘轮廓点
+    findContours(gray, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+
+    vector<Point> approx;
+    for (vector<Point> points : contours) {
+        approxPolyDP(points, approx, arcLength(points, true) * 0.02, true);
+        if (approx.size() == 4 &&
+            fabs(contourArea(approx)) > 1000 &&
+            isContourConvex(approx) &&
+            isCntValid(approx, gray.rows, gray.cols) &&
+            // 判断
+            isCntScaleValid(approx)) {
+            double maxCosine = 0;
+
+            for (int j = 2; j < 5; j++) {
+                double angle = angleOfPoint(approx[j%4], approx[j-2], approx[j-1]);
+                double cosine = fabs(angle);
+                maxCosine = MAX(maxCosine, cosine);
+            }
+            // 判断两个边的夹角接近90度垂直
+            if (maxCosine < 0.3) {
+                // 计算面积最大的四边形
+                squares.push_back(approx);
+            }
+        }
+    }
+}
+
+vector<vector<Point>> detect_square(const Mat mat, const int gauss_filter_size = 15) {
+    vector<vector<Point> > squares;
+    Mat destination, gray;
+
+    vector<vector<Point> > contours;
+
+    // 2. Gauss模糊处理
+    GaussianBlur(mat, destination, Size(gauss_filter_size, gauss_filter_size), 0);
+    //    imwrite("/Users/jw.hu/Desktop", destination);
+    imshow("blur", destination);
+    // 3. 通过canny找边线，或者黑白阈值
+    for (int l = 0; l < 255; l += 5) {
+        if (l == 0) {
+            Canny(destination, gray, 0, 50, 7);
+            imshow("canny", gray);
+            dilate(gray, gray, Mat());
+        } else {
+            threshold(destination, gray, l, 255, THRESH_BINARY);
+        }
+
+        // 找到形状的边缘轮廓点
+        traverse_points_find_square(gray, contours, squares);
+    }
+    return squares;
+}
 void imageOperator() {
-    Mat img = imread("/Users/jw.hu/Desktop/OpenCV_Source/apple.jpg");
+    Mat img = imread("/Users/mac/Desktop/OpenCV_Source/pic3.png");
     imshow("source", img);
     
     Mat gray;
     cvtColor(img, gray, CV_BGR2GRAY);
     imshow("gray", gray);
     
+    vector<vector<cv::Point>> pointArray = detect_square(gray);
+    drawContours(img, pointArray, 50, Scalar(0, 255, 0));
+    imshow("draw", img);
     // 轮廓
-    Mat canny;
-    Canny(gray, canny, 200, 500);
-    imshow("canny", canny);
+//    Mat canny;
+//    Canny(gray, canny, 200, 500);
+//    imshow("canny", canny);
     
     waitKey();
 }
