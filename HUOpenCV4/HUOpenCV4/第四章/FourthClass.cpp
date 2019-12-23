8284dcfaf31a08ea9f7a37d5497e2a65defd2d4c
Hu Jiawen <jw.hu@s2icode.com>
2019-12-23 09:36:40 +0800

    第四章

--- HUOpenCV4/HUOpenCV4/第四章/FourthClass.cpp
+++ HUOpenCV4/HUOpenCV4/第四章/FourthClass.cpp
@@ -8,6 +8,8 @@
 
 #include "FourthClass.hpp"
 #include <opencv.hpp>
+using namespace cv;
+using namespace std;
 
 void mat_test() {
     cv::Mat mat(300, 1000, 3.f, CV_32FC3);
@@ -28,6 +30,24 @@ void mat_copy() {
     cv::Range rangeCol(10, 210);
     cv::Mat matRange(apple, rangeRow, rangeCol);
     cv::imshow("range", matRange);
+    
+    cout << (int)apple.at<cv::Vec3b>(10, 10)[0] << endl;
+    cout << apple.at<cv::Vec3b>(10, 10)[1] << endl;
+    cout << (int)apple.at<cv::Vec3b>(10, 10)[2] << endl;
+
     cv::waitKey();
 
+}
+
+void example_4_1() {
+    const int n_mat_size = 5;
+    const int n_mat_sz[] = { n_mat_size, n_mat_size, n_mat_size};
+    Mat n_mat( 3, n_mat, CV_32FC1);
+    
+    cv::RNG rng;
+    rng.fill(n_mat, cv::RNG::UNIFORM, 0.f, 1.f);
+    
+    const Mat* arrays[] = { &n_mat, 0 };
+    Mat my_planes[1];
+    NAryMatIterator it (arrays, my_planes);
 }
